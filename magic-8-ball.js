f5e2280b4d115c26f9abd6202bbc49a6daa7b8dc
Stephanie Blackman <stephaniemblackman@gmail.com>
2025-07-07 18:59:16 -0600

    correcting rock paper scissors program

--- magic-8-ball.js
+++ magic-8-ball.js
@@ -1,19 +1,43 @@
+const prompt = require("prompt-sync")();
+
 function playGame(userChoice) {
   const choices = ["rock", "paper", "scissors"];
-  const computerChoice = choices[Math.floor(Math.random() * 3)];
 
-  console.log(`You chose: ${userChoice}`);
-  console.log(`Computer chose: ${computerChoice}`);
+  while (true) {
+    let userChoice = prompt(
+      "Rock, paper, or scissors? (type 'stop' to quit)  "
+    );
+    userChoice = userChoice.trim().toLowerCase();
 
-  if (userChoice === computerChoice) {
-    console.log("It's a draw!");
-  } else if (
-    (userChoice === "rock" && computerChoice === "scissors") ||
-    (userChoice === "paper" && computerChoice === "rock") ||
-    (userChoice === "scissors" && computerChoice === "paper")
-  ) {
-    console.log("You win!");
-  } else {
-    console.log("You lose!");
+    if (userChoice === "stop" || userChoice === "quit") {
+      console.log("\nCome play again soon!");
+      break;
+    }
+    if (!choices.includes(userChoice)) {
+      console.log("\nInvalid. Please type rock, paper, or scissors.\n");
+      continue;
+    }
+
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    console.log(
+      `\nYou chose: ${userChoice}\nComputer chose: ${computerChoice}`
+    );
+
+    if (userChoice === computerChoice) {
+      console.log("It's a tie!\n");
+      break;
+    } else if (
+      (userChoice === "rock" && computerChoice === "scissors") ||
+      (userChoice === "paper" && computerChoice === "rock") ||
+      (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+      console.log("You win!");
+      break;
+    } else {
+      console.log("You lose!\n");
+      break;
+    }
   }
 }
+
+playGame();
